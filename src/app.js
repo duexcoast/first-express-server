aa95ab6754e0c57df811ef48ff5f72f17a34c0b4
uxconor <conor.ux@gmail.com>
2022-07-14 12:18:36 -0400

    hbs routing and 404 pages

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 const path = require('path');
-
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
+
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
+// Setup handlebars engine and views location
 app.set('view enginge', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
+//
 app.get('', (req, res) => {
   res.render('index.hbs', {
     title: 'Weather App',
@@ -17,24 +26,32 @@ app.get('', (req, res) => {
 });
 
 app.get('/help', (req, res) => {
-  res.send([
-    {
-      age: 29,
-      name: 'Conor',
-    },
-    {
-      age: 26,
-      name: 'Dana',
-    },
-  ]);
+  res.render('help.hbs', {
+    title: 'Help',
+    message: 'I need somebody!',
+  });
+});
+app.get('/help/*', (req, res) => {
+  res.render('help/404.hbs', {
+    error: "Hmm... It seems we can't help you",
+  });
 });
 app.get('/about', (req, res) => {
-  res.send('About');
-});
-app.get('/weather', (req, res) => {
-  res.send('Weather');
+  res.render('about.hbs', {
+    title: 'Help',
+    title: 'About',
+    name: 'Conor Ney',
+  });
 });
 
+app.get('*', (req, res) => {
+  res.render('404.hbs', {
+    title: "Weather App"
+    error: "Could not find the page you're looking for",
+  });
+});
+
+// Activate Server
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
 });
